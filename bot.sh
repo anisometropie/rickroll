47692868cae25034d28e7559afa2e719fc071c91
Valentin Chanas <anisometropie@gmail.com>
2021-11-01 22:48:05 +0100

    refactor

--- bot.sh
+++ bot.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 input=$1
 
+printChar () {
+  printf "$1" | xclip -i -selection clipboard
+  xdotool key --clearmodifiers "ctrl+v"
+}
+
 count=0
 while IFS= read -r line; do
   for (( i=0; i<${#line}; i++ )); do
     char=${line:$i:1}
-    printf "$char" | xclip -i -selection clipboard
-    xdotool key --clearmodifiers "ctrl+v"
+    printChar "$char"
     ((count++))
     sleep 0.001
   done
-  printf "\n" | xclip -i -selection clipboard
-  xdotool key --clearmodifiers "ctrl+v"
+  printChar "\n"
   while [ $count -gt 0 ]; do
     echo $count
     xdotool key Left
